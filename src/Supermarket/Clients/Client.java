c795e9840cdd41dce8f479dc1c9b2e096dd6d1ec
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-05-02 10:04:42 +0100

    feat: se ha creado la clase products y clients en la carpeta Supermarket

---
+++ src/Supermarket/Clients/Client.java
@@ -0,0 +1,4 @@
+package Supermarket.Clients;
+
+public class Client {
+}
---
+++ src/Supermarket/Products.java
@@ -0,0 +1,7 @@
+package Supermarket;
+
+public class Products {
+    private static String aviableProducts[] = {"manzana", "tomate", "leche", "cereales", "pan", "lejia", "limpiacristales", "atun", "aceite", "cerveza", "huevos", "refrescos", "azucar", "harina"};
+
+
+}
