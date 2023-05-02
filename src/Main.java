482b5890e31f6880af15cbcd9ce3c3f430ea647f
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-05-02 10:22:38 +0100

    faddProducts): se añadio el metodo randomProduct que retorna un producto aleatorio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+import Supermarket.Products;
+
 public class Main {
 
     public static void main(String[] args) {
-
     }
 }
--- src/Supermarket/Products.java
+++ src/Supermarket/Products.java
@@ -4,4 +4,8 @@ public class Products {
     private static String aviableProducts[] = {"manzana", "tomate", "leche", "cereales", "pan", "lejia", "limpiacristales", "atun", "aceite", "cerveza", "huevos", "refrescos", "azucar", "harina"};
 
 
+    public String randomProduct() {
+        return aviableProducts[(int)Math.floor(Math.random()*14)];
+    }
+
 }
