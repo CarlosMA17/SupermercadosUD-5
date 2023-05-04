ff606cb809a190346c239296c80bf040c5c88ecb
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-05-04 19:11:55 +0100

    add: se han añadido los atributos a la clase cashier

--- src/Supermarket/Clients/Cashier/Cashier.java
+++ src/Supermarket/Clients/Cashier/Cashier.java
@@ -1,4 +1,8 @@
 package Supermarket.Clients.Cashier;
+import Supermarket.Clients.Client;
 
 public class Cashier {
+    private int cashierNumber;
+    private int totalClients;
+    private Client[] clients;
 }
