56562fa8475a0ace16660de7166e2c0e8ed21c57
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-05-04 18:50:36 +0100

    add(Products): se ha añadido los veinte nombres al array de personas

--- src/Supermarket/Products.java
+++ src/Supermarket/Products.java
@@ -2,10 +2,13 @@ package Supermarket;
 
 public class Products {
     private static String aviableProducts[] = {"manzana", "tomate", "leche", "cereales", "pan", "lejia", "limpiacristales", "atun", "aceite", "cerveza", "huevos", "refrescos", "azucar", "harina"};
+    private static String people[] = {"Juan", "Carlos", "Diego", "Pedro", "Carmen", "Paola", "Paula", "Ramiro", "Alvaro", "Gwyn", "Anri", "Solaire", "Nito", "Isalith", "Gwyndolin", "Artorias", "Sith", "Siegward", "Crisanta", "Catalina"};
 
-
-    public String randomProduct() {
+    public static String randomProduct() {
         return aviableProducts[(int)Math.floor(Math.random()*14)];
     }
 
+    public static String randomPerson() {
+        return people[(int)Math.floor(Math.random()*20)];
+    }
 }
