b8166b11abe716154a9bba121ad6c53dc2c8a326
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-05-04 19:34:56 +0100

    se ha añadido un swicth para las opciones del cajero

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
 import supermarket.Products;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Products.randomPerson());
+        boolean exit = true;
+
+        while(exit) {
+            System.out.println("elija una opcion:\n" +
+                    "1.- Abrir caja\n" +
+                    "2.- Añadir un nuevo cliente a la cola\n" +
+                    "3.- Atender al cliente\n" +
+                    "4.- Ver clientes pendientes\n" +
+                    "5.- Cerrar supermercado");
+            Scanner sc = new Scanner(System.in);
+            String option = sc.nextLine();
+            switch (option) {
+                case "1":
+                    System.out.println(1);
+                    break;
+                case "2":
+                    System.out.println(2);
+                    break;
+                case "3":
+                    System.out.println(3);
+                    break;
+                case "4":
+                    System.out.println(4);
+                    break;
+                case "5":
+                    System.out.println(5);
+                    break;
+                default:
+                    System.out.println(0);
+                    break;
+            }
+        }
     }
 }
