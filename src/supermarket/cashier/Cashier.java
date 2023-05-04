ee2a38f8ddeb6684081689c67500be1c1532debf
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-05-04 19:22:03 +0100

    feat: se ha añadido los metodos para mostrar las clases client y cashier por pantalla

--- src/supermarket/cashier/Cashier.java
+++ src/supermarket/cashier/Cashier.java
@@ -1,8 +1,21 @@
 package supermarket.cashier;
 import supermarket.clients.Client;
 
+import java.util.Arrays;
+
 public class Cashier {
     private int cashierNumber;
     private int totalClients;
     private Client[] clients;
+
+    @Override
+    public String toString() {
+        return "===================================\n" +
+                "* Número de caja: <" + this.cashierNumber + ">:\n" +
+                "* Total de clientes: <" + this.totalClients + ">\n" +
+                "* Clientes en la fila:\n" +
+                "<" + this.clients[0].getName() + ">\n" +
+                "<" + this.clients[1].getName() + ">\n" +
+                "===================================";
+    }
 }
--- src/supermarket/clients/Client.java
+++ src/supermarket/clients/Client.java
@@ -1,7 +1,24 @@
 package supermarket.clients;
 
+import java.util.Arrays;
+
 public class Client {
     private String name;
     private int totalProducts;
     private String[] articles;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "===================================\n" +
+                "* Nombre: <" + this.getName() + ">\n" +
+                "* Total de productos: <" + this.totalProducts + ">\n" +
+                "* Lista de artículos en la cesta:\n" +
+                "<" + this.articles[0] + ">\n" +
+                "<" + this.articles[1] + ">\n" +
+                "===================================";
+    }
 }
